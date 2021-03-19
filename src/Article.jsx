b205eaa83e46af52a39d491cf3139666e4a46b27
shinji0320 <shinji.blue.one@gmail.com>
2021-03-19 09:00:46 +0900

    ライフサイクルの学習

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LikeButton from './LikeButton';
 
 const Article = (props) => {
 
@@ -7,6 +8,7 @@ const Article = (props) => {
       <h2>{props.title}</h2>
       <label htmlFor="check">公開状態: </label>
       <input type="checkbox" checked={props.isPublished} id="check" onClick={() => props.toggle()}/>
+      <LikeButton count={props.count} />
     </div>
   )
 };
--- src/Blog.js
+++ src/Blog.js
@@ -6,20 +6,45 @@ class Blog extends React.Component {
     super(props);
     this.state = {
       isPublished: false,
-      order: 1
+      count: 0
     }
   }
 
+  componentDidMount() {
+    //ボタンがクリックされたらいいねをカウントアップする
+    document.getElementById('counter').addEventListener('click', this.countUp)
+  }
+
+  componentDidUpdate() {
+    if (this.state.count >= 10) {
+      this.setState({ count: 0 })
+    }
+  }
+
+  componentWillUnmount() {
+    document.getElementById('counter').removeEventListener('click', this.countUp)
+  }
+
+  // 公開状態を反転させる関数
   togglePublished = () => {
     this.setState({
       isPublished: !this.state.isPublished
     })
   };
+
+  countUp = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
   render() {
-    const authorName = "FaceBook"
     return (
       <>
-        <Article title={"Reactの入門"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()}/>
+        <Article
+            title={"Reactの入門"}
+            isPublished={this.state.isPublished}
+            toggle={() => this.togglePublished()}
+            count={this.state.count}
+         />
       </>
     )
   }
